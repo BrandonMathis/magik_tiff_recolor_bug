69c61d16c0d3a14286db06c183a0133135506368
Brandon Mathis <bemathis@gmail.com>
2015-08-06 20:50:34 -0400

    Errors demo

---
+++ recolor_errors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+convert black_square.tif -fill "#8649ba" -opaque "#FFFFFF" magenta_out.tif
+convert black_square.tif -fill "#a9f02a" -opaque "#FFFFFF" yellow_out.tif
+convert black_square.tif -fill "#6df02a" -opaque "#FFFFFF" green_out.tif
+convert black_square.tif -fill "#f0702a" -opaque "#FFFFFF" red_out.tif
+convert black_square.tif -fill "#2ae1f0" -opaque "#FFFFFF" cyan_out.tif
+convert black_square.tif -fill "#2a73f0" -opaque "#FFFFFF" blue_out.tif
